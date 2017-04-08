622e71a4fb462edd1702e9af5157d731d3ccbdf7
Nefeldaiel <nefeldaiel@gmail.com>
2017-04-08 20:42:30 +0800

    aspects for sequence parser

--- java-uml-parser/src/main/java/java_uml_seq_parser/MyJavaSeqParser.java
+++ java-uml-parser/src/main/java/java_uml_seq_parser/MyJavaSeqParser.java
@@ -1,5 +1,17 @@
 package java_uml_seq_parser;
 
+import java_uml_seq_parser.core.SequenceParser;
+import java_uml_seq_parser.core.SequenceParserImpl;
+
 public class MyJavaSeqParser {
 
+	public static void main(String[] args) {
+		final SequenceParser parser = new SequenceParserImpl();
+		System.out.println("Demo1: method without argument");
+		parser.parse();
+		System.out.println();
+		
+		System.out.println("Demo2: method with argument");
+		parser.parseWithArgument("Feifei-is-cute");
+	}
 }
